6c260a719821607eccbbd389fde048a33686d7dd
NikhileshKS <nickstack09@gmail.com>
2024-10-14 00:12:17 +0530

    new feature

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
-
+import { Route,Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Doctors from './pages/Doctors'
+import Login from './pages/Login'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Myprofile from './pages/Myprofile'
+import MyAppointments from './pages/MyAppointments'
+import Appointment from './pages/Appointment'
+import Navbar from './components/Navbar'
 const App = () => {
   return (
-    <div className='text-stone-500'>
-      MyDoctor
+    <div className='mx-4 sm:mx-[10]'>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/doctors' element={<Doctors/>}/>
+      <Route path='/doctors/:speciality' element={<Doctors/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/Contact' element={<Contact/>}/>
+      <Route path='/Myprofile' element={<Myprofile/>}/>
+      <Route path='/MyAppointments' element={<MyAppointments/>}/>
+      <Route path='/Appointments/:docId' element={<Appointment/>}/>
+
+
+    </Routes>
     </div>
   )
 }
